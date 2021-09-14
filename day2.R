2c07fca0b0aa4028ec71f0f069ee804a44ea313d
wambu2021 <90614249+wambu2021@users.noreply.github.com>
2021-09-14 13:23:56 +0100

    Add files via upload

---
+++ day2.R
@@ -0,0 +1,65 @@
+x=3
+y=5
+
+number = x +y
+
+print(number)
+
+x = 5
+
+number = x+y
+print(number)
+y=10
+number=y+x
+print(number)
+
+hPrice<-c(5000000,4500000,360000)
+hPrice
+
+hQuality<-c("good", "Average", "Bad")
+hQuality
+
+m <- c(hPrice, hQuality)
+
+hQuality<-c("Good", "Average", "Bad","Good", "Average", "Bad")
+hQuality<-factor(hQuality)
+
+df<-data.frame(hPrice, hQuality)
+list<-list(hPrice, df,hQuality)
+a<-" love Essex"
+print(a)
+b<-" love colchester Campus"
+print(b)
+c<-"I parked my car' near North Building"
+print(c)
+a<-3 
+b<-10
+if(b>a) {print("b is greater than a")}
+
+a<-10
+b<-10
+if(b>a)
+
+
+install.packages("tidyverse")
+library(tidyverse)
+x<-c("Apple", "Banana", "Pear")
+str_sub(x, 1, 3 )
+str_sub(x, -3, -1)
+
+5>2
+5==5
+a<-100
+b<-90
+a>b
+
+for (x in 2:20) {print(x)}
+
+my_animals <- list("dogs", "cows", "Zebras")
+
+for(x in my_animals){print(x)
+
+
+my
+
+
